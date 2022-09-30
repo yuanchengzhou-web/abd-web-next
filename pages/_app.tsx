ff986b354a04fb8d2c3adc269ce137ecfbb6b92d
michael zhou <yuanchengzhou123@hotmail.com>
2022-09-30 18:12:26 -0400

    fix: install husky and prettier properly

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,19 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head';
 
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(()=>{
+  useEffect(() => {
     require('@/styles/template/animate.css');
     require('@/styles/template/font-awesome.min.css');
     require('@/styles/template/magnific-popup.css');
     require('@/styles/template/owl.carousel.css');
     require('@/styles/template/themify-icons.css');
     require('@/styles/template/bootstrap.min.css');
-  })
+  }
+  )
   return <Component {...pageProps} />
 }
 
