b1bebae59b33eb7b9e2b51e03e7d79f2e4a59ea2
michael zhou <yuanchengzhou123@hotmail.com>
2022-09-30 20:20:04 -0400

    refactor: put pages and styles inside the src folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,17 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import { useEffect } from 'react';
 
-
 export default function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
+    require('bootstrap/dist/js/bootstrap');
     require('@/styles/template/animate.css');
-    require('@/styles/template/font-awesome.min.css');
     require('@/styles/template/magnific-popup.css');
-    require('@/styles/template/owl.carousel.css');
-    require('@/styles/template/themify-icons.css');
+    //require('@/styles/template/owl.carousel.css');
+    //require('@/styles/template/themify-icons.css');
     require('@/styles/template/bootstrap.min.css');
-  }
-  )
-  return <Component {...pageProps} />
+    //require('@/styles/template/styles.css');
+  });
+  return <Component {...pageProps} />;
 }
-
